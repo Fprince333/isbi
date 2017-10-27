e9702a1ad2d1b735330d8677ce7ae617329f2a07
Will Smith <lwilsonsmith@gmail.com>
2017-10-27 19:48:02 -0400

    Remove now useless css and js

--- custom.js
+++ custom.js
@@ -17,15 +17,6 @@ $j(document).ready(function() {
 	$j('.passive .counter').append('%');
 	$j('.side_menu_button_wrapper').prepend('<p class="search-text">SEARCH</p>');
 
-	$j('.about-link').on('click', function() {
-		$j('html, body').animate(
-			{
-				scrollTop: $j('#about').offset().top
-			},
-			800
-		);
-	});
-
 	$j('.search-text').on('click', function() {
 		$j('.form_holder_outer').css('height', $j('.header_inner').height() + 'px');
 		$j("form[role='search']").animate({ opacity: 'show' });
@@ -35,31 +26,8 @@ $j(document).ready(function() {
 		$j("form[role='search']").animate({ opacity: 'hide' });
 	});
 
-	$j(window).scroll(function() {
-		if (window.location.pathname.length === 1) {
-			$j.each($j('#menu-header li a'), function(i, v) {
-				$j(v).removeClass('active');
-			});
-			if (isScrolledIntoView('.homepage-top-row') || isScrolledIntoView('.counter-row')) {
-				$j('#menu-header li.home').addClass('active');
-			}
-			if (isScrolledIntoView('#contact-us')) {
-				$j('#menu-header li.contact').addClass('active');
-			} else {
-				$j('#menu-header li.contact').removeClass('active');
-				$j('#menu-header li.contact').removeClass('current_page_item');
-			}
-		}
-	});
-
 	$j('#portfolio').on('mouseleave', function() {
 		$j('#secondary-chart-container').hide();
 		$j('#portfolio-text').show();
 	});
-
-	if (window.location.pathname.length === 1) {
-		$j.each($j('#menu-header li a'), function(i, v) {
-			$j(v).removeClass('active');
-		});
-	}
 });
